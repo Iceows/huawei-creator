76f01536bf38ac50bf855400ca9abb48bd8c0dcb
Raphael Mounier <mounierr07@gmail.com>
2022-11-04 22:48:23 +0100

    Add google variant

--- build-all.sh
+++ build-all.sh
@@ -8,12 +8,21 @@ origin="$(dirname "$origin")"
 
 
 
-srcFile="$1"
+srcDateFile="$1"
 
 
 cd ../leaos-aosp
-bash aosp_build_leaos/build.sh treble 64BVS
-bash aosp_build_leaos/build.sh treble nosync 64BGS
+#bash aosp_build_leaos/build.sh treble 64BVS
+#bash aosp_build_leaos/build.sh treble nosync 64BGS
 
 cd ../huawei-creator
-bash sudo run-huawei-ab-a13.sh ${srcFile} "LeaOS" "ANE-LX1" "Y"
+
+# Vanilia
+sudo bash ./run-huawei-ab-a13.sh /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvS.img "LeaOS" "ANE-LX1" "Y"
+mv s-vndklite.img /media/iceows/Sauvegardes/ice-rom/LeaOS-AOSP/LeaOS-A13-${srcDateFile}-iceows-ane.img
+xz -z /media/iceows/Sauvegardes/ice-rom/LeaOS-AOSP/LeaOS-A13-${srcDateFile}-iceows-ane.img
+
+# Google
+sudo bash ../run-huawei-ab-a13.sh /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgS.img "LeaOS" "ANE-LX1" "Y"
+mv s-vndklite.img /media/iceows/Sauvegardes/ice-rom/LeaOS-A13-${srcDateFile}-iceows-google-ane.img
+xz -z /media/iceows/Sauvegardes/ice-rom/LeaOS-A13-${srcDateFile}-iceows-google-ane.img
