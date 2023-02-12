9137ed9007dcd3953269763f47a42f6d673abf73
Raphael Mounier <mounierr07@gmail.com>
2023-02-12 17:11:28 +0100

    Feb 2023

--- build-all.sh
+++ build-all.sh
@@ -22,24 +22,25 @@ cd ../huawei-creator
 # TrebleDroid ab version
 cp /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.img /media/iceows/Sauvegardes/ice-rom/TrebleDroid/
 cp /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.img /media/iceows/Sauvegardes/ice-rom/TrebleDroid/
-xz -c /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.xz
-xz -c /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.xz
+
+xz -kv0 -T16 /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.xz
+xz -kv0 -T16 /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.xz
 
 # TrebleDroid vndklite version
 sudo bash ./lite-adapter.sh 64 /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.img
 mv s-vndklite.img /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vvN.img
-xz -c /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vvN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vvN.xz
+xz -kv0 -T16 /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vvN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vvN.xz
 sudo bash ./lite-adapter.sh 64 /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.img
 mv s-vndklite.img /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vgN.img
-xz -c /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vgN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vgN.xz
+xz -kv0 -T16 /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vgN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_vgN.xz
 
 # TrebleDroid erofs version
 sudo bash ./make-erofs.sh /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bvN.img
 mv s-erofs.img /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_evN.img
-xz -c /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_evN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_evN.xz
+xz -kv0 -T16  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_evN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_evN.xz
 sudo bash ./make-erofs.sh /home/iceows/build-output/TrebleDroid-A13-${srcDateFile}-treble_arm64_bgN.img
 mv s-erofs.img /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_egN.img
-xz -c /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_egN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_egN.xz
+xz -kv0 -T16 /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_egN.img -T0 >  /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_arm64_egN.xz
 
 
 # --------------------------------- LeaOS Gsis -----------------------------------
--- sftp-all.sh
+++ sftp-all.sh
@@ -11,14 +11,14 @@ srcDateFile="$1"
 
 # LeaOS Vanilia + Google
 for model in ane fig stf cor vtr pot;do
-	lftp sftp://altairfr:xxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-A13 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS-AOSP/A13/LeaOS-A13-${srcDateFile}-iceows-${model}.xz; bye"
-	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-A13 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS-AOSP/A13/LeaOS-A13-${srcDateFile}-iceows-google-${model}.xz; bye"
-done
+ 	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-A13 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS-AOSP/A13/LeaOS-A13-${srcDateFile}-iceows-${model}.xz; bye"
+ 	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-A13 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS-AOSP/A13/LeaOS-A13-${srcDateFile}-iceows-google-${model}.xz; bye"
+ done
 
 
 # LeaOS-GSI Vanilia + Google
 for model in arm64_bvN arm64_bgN arm64_evN arm64_egN arm64_vvN arm64_vgN;do
-	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/TrebleDroid-GSI ;put /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_${model}.xz; bye"
+	lftp sftp://altairfr:xxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/TrebleDroid-GSI ;put /media/iceows/Sauvegardes/ice-rom/TrebleDroid/TrebleDroid-A13-${srcDateFile}-treble_${model}.xz; bye"
 done
 
 
