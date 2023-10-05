5103777bef5934aa883d8f4ba8292d81625fbc27
Raphael Mounier <mounierr07@gmail.com>
2023-10-05 13:33:37 +0200

    sftp: lineage build remove pot

--- sftp-lineage-all.sh
+++ sftp-lineage-all.sh
@@ -10,7 +10,7 @@ srcDateFile="$1"
 
 
 # LeaOS Vanilia + Google
-for model in ane fig stf vtr pot;do
+for model in ane fig stf vtr;do
  	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-iceows-${model}.img.xz; bye"
  	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-iceows-google-${model}.img.xz; bye"
  done
