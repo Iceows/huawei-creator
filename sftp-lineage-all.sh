c4049278b3616405b6a95beb79183e00e60a5bf7
Raphael Mounier <mounierr07@gmail.com>
2024-03-24 14:51:25 +0100

    sftp: add STK to sftp lineage all

--- sftp-lineage-all.sh
+++ sftp-lineage-all.sh
@@ -10,13 +10,13 @@ srcDateFile="$1"
 
 
 # LeaOS Vanilia + Google
-for model in ane fig stf vtr;do
- 	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-iceows-${model}.img.xz; bye"
- 	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-iceows-google-${model}.img.xz; bye"
+for model in ane fig stf vtr cor stk;do
+ 	lftp sftp://altairfr:xxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-iceows-${model}.img.xz; bye"
+ 	lftp sftp://altairfr:xxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-iceows-google-${model}.img.xz; bye"
  done
 
 # LeaOS-GSI Vanilia + Google
 for model in arm64_bvN arm64_bgN;do
-	lftp sftp://altairfr:xxxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-${model}.img.xz; bye"
+	lftp sftp://altairfr:xxx@frs.sourceforge.net -e "cd /home/frs/project/altairfr-huawei/LeaOS-20.0 ;put /media/iceows/Sauvegardes/ice-rom/LeaOS/20.0/LeaOS-20.0-${srcDateFile}-${model}.img.xz; bye"
 done
 
