107beb96ec4b17a80f328f0beab668e70978bfa3
Raphael Mounier <mounierr07@gmail.com>
2025-11-06 19:24:39 +0100

    run: add treble app apk

--- run-huawei-emui9-ab-a15.sh
+++ run-huawei-emui9-ab-a15.sh
@@ -209,17 +209,20 @@ mount -o loop,rw s-ab-raw.img d
 	cp "$origin/files-patch/system/etc/init/debug-log-gsi.rc" "etc/init/debug-log-gsi.rc"
 	xattr -w security.selinux u:object_r:system_file:s0 "etc/init/debug-log-gsi.rc"
 		
-	# -----------------------------PHH Exec ------------------------------ #
+	# -----------------------------PHH Exec -------------------------------- #
 	cp "$origin/files-patch/system/bin/rw-system.sh" "bin/rw-system.sh"
 	xattr -w security.selinux u:object_r:phhsu_exec:s0 "bin/rw-system.sh"
 	
-	# -----------------------------Policy------------------------------- #
+	# -----------------------------Policy----------------------------------- #
 	cp "$origin/files-patch/system/etc/seccomp_policy/configstore@1.1.policy" "etc/seccomp_policy/configstore@1.1.policy"
 	chmod 644 "etc/seccomp_policy/configstore@1.1.policy"
 	xattr -w security.selinux u:object_r:system_seccomp_policy_file:s0 "etc/seccomp_policy/configstore@1.1.policy"
-	
-        # rm -rf "etc/seccomp_policy/crash_dump.arm64.policy"
-        # rm -rf "etc/seccomp_policy/crash_dump.arm.policy"
+
+	cp "$origin/files-patch/system/etc/seccomp_policy/mediacodec.policy" "etc/seccomp_policy/mediacodec.policy"
+	chmod 644 "etc/seccomp_policy/mediacodec.policy"
+	xattr -w security.selinux u:object_r:system_seccomp_policy_file:s0 "etc/seccomp_policy/mediacodec.policy"
+
+
 
 	#-----------------------------File copy -----------------------------------------------------
 
